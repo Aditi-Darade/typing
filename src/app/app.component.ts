5797a4204b2986e72efea2245938547a2898a08e
Aditi-Darade <aditidarade1@gmail.com>
2024-10-30 15:19:25 -0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {lorem} from 'faker';
+import { NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typing';
+
+  randomText = lorem.sentence();
+  enteredText ='';
+  onInput(event: Event)
+  {
+    const target = event.target as HTMLInputElement; //?
+    this.enteredText = String(target.value) //?
+    console.log(this.enteredText);
+    
+  }
+  compare(letter: string, enteredText: string)
+  {
+    if (!enteredText)
+      {
+        return 'pending';
+      }
+    if(enteredText == letter)
+    {
+      return 'match';
+    }
+    else 
+    {
+      return 'notMatch';
+    }
+  }
+  /*
+  generateRandomSentence()
+  {
+     var randomSentence = '';
+     const letters = 'abcdefghijklmnopqrstuvwxyz';
+    for (let i=0; i <Math.floor(Math.random() * 10); i++ )
+    {
+      randomSentence = randomSentence + letters[Math.floor(Math.random() * 26)];
+    }
+    return randomSentence;
+  }
+  */
 }
